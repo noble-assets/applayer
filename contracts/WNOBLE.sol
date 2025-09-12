2aaa6b1edff130b7f01657d29a08be0ada920123
John Letey <john@noble.xyz>
2025-09-12 21:32:46 +0200

    chore: WETH -> WNOBLE (again)

--- contracts/WNOBLE.sol
+++ contracts/WNOBLE.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.4;
 
 import {ERC20} from "./ERC20.sol";
 
-/// @notice Simple Wrapped Ether implementation.
+/// @notice Wrapped Noble
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/tokens/WETH.sol)
 /// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/WETH.sol)
 /// @author Inspired by WETH9 (https://github.com/dapphub/ds-weth/blob/master/src/weth9.sol)
-contract WETH is ERC20 {
+contract WNOBLE is ERC20 {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                       CUSTOM ERRORS                        */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /// @dev The ETH transfer has failed.
-    error ETHTransferFailed();
+    /// @dev The NOBLE transfer has failed.
+    error NOBLETransferFailed();
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                       ERC20 METADATA                       */
@@ -21,31 +21,40 @@ contract WETH is ERC20 {
 
     /// @dev Returns the name of the token.
     function name() public view virtual override returns (string memory) {
-        return "Wrapped Ether";
+        return "Wrapped Noble";
     }
 
     /// @dev Returns the symbol of the token.
     function symbol() public view virtual override returns (string memory) {
-        return "WETH";
+        return "WNOBLE";
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-    /*                            WETH                            */
+    /*                          EIP-2612                          */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    /// @dev Deposits `amount` ETH of the caller and mints `amount` WETH to the caller.
+    /// @dev Returns the constant name hash of the token.
+    function _constantNameHash() internal view virtual override returns (bytes32 result) {
+        return 0xbaf4cc79d53a03a5e2e4af1a7c9994baa9be6c65e30fa46eb28366a45d5fd3de;
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                           WNOBLE                           */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @dev Deposits `amount` NOBLE of the caller and mints `amount` WNOBLE to the caller.
     function deposit() public payable virtual {
         _mint(msg.sender, msg.value);
     }
 
-    /// @dev Burns `amount` WETH of the caller and sends `amount` ETH to the caller.
+    /// @dev Burns `amount` WNOBLE of the caller and sends `amount` NOBLE to the caller.
     function withdraw(uint256 amount) public virtual {
         _burn(msg.sender, amount);
         /// @solidity memory-safe-assembly
         assembly {
-            // Transfer the ETH and check if it succeeded or not.
+            // Transfer the NOBLE and check if it succeeded or not.
             if iszero(call(gas(), caller(), amount, codesize(), 0x00, codesize(), 0x00)) {
-                mstore(0x00, 0xb12d13eb) // `ETHTransferFailed()`.
+                mstore(0x00, 0x1ea40fde) // `NOBLETransferFailed()`.
                 revert(0x1c, 0x04)
             }
         }
